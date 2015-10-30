7fb187a84b091d5cedead91d2198ec560e3eff07
Ivan Samorodov <gsitex@gmail.com>
2015-10-31 01:06:03 +0700

    use dispatchShell

---
+++ Console/Command/HerokuShell.php
@@ -0,0 +1,50 @@
+<?php
+
+App::uses('AppShell', 'Console/Command');
+
+/**
+ * Heroku Shell
+ *
+ * @category Shell
+ * @version  1.0
+ * @author   Ivan Samorodov <gsitex@gmail.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php The MIT License
+ * @link     http://www.sitex.kz
+ */
+class HerokuShell extends AppShell
+{
+
+	public function main()
+	{
+		$this->out();
+		$this->out('Collecting data');
+
+		$db_parsed = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+		$admin_user = $_ENV['ADMIN_USER'];
+		$admin_password = $_ENV['ADMIN_PASSWORD'];
+
+		$this->out('Dispatching InstallShell');
+
+		$this->dispatchShell(
+			'install.install',
+			'-d',
+			'Mysql',
+			'-h',
+			$db_parsed['host'],
+			'-l',
+			$db_parsed['user'],
+			'-p',
+			$db_parsed['pass'],
+			'-n',
+			substr($db_parsed['path'], 1),
+			'-t',
+			'3306',
+			'-x',
+			'croogo_',
+			$admin_user,
+			$admin_password
+		);
+
+	}
+
+}
